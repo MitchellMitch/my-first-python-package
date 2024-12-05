e104d8c800fbecafdc1e37094b55add8954cca9b
Michael Schötz <michael.schoetz@th-deg.de>
2024-12-05 13:52:54 +0000

    added basic details

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ except FileNotFoundError:
 
 # Metadata
 setup(
-  name = "Type here the name of your package",
+  name = "michaelpy",
   version = 0.0.0.9000,
-  author = "Type here your name",
-  author_email = "Type here your email",
-  description = "Describe here your package in one sentence",
+  author = "Michael Schötz",
+  author_email = "michael.schoetz@th-deg.de",
+  description = "Demo Python package",
   license = "Type here what license your package has",
   packages = find_packages(),
   install_requires = requirements
