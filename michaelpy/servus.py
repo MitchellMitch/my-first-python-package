1c9c1e0d451e4083eec6e314f7de64eefeb8f15c
Michael Schötz <michael.schoetz@th-deg.de>
2024-12-05 14:10:16 +0000

    documented function

--- michaelpy/servus.py
+++ michaelpy/servus.py
@@ -1,2 +1,21 @@
 def servus(name = "world"):
+  """Servus
+  
+  This function will great you in a bavarian way.
+  
+  Parameters
+  ----------
+  name : str
+    The name of the person to be greated.
+
+  Returns
+  -------
+  None
+    Only prints to the console.
+  
+  Examples
+  --------
+  >>> servus("Hias")
+  >>> Servus Hias! ....
+  """
   print(f"Servus {name}! Welcome to my first Python package!")
